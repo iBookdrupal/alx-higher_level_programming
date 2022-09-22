6a263c42738f84c7e4914857a95ceadccfbe1625
iBookdrupal <ternguh@gmail.com>
2022-09-22 05:11:16 +0100

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = number % (-10)
+    last_digit = ((number * -1) % 10) * -1
 else:
     last_digit = number % (10)
 
